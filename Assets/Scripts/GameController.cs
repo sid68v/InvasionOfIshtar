706b8addb92df0f61e86f61ad238f3f992c07614
sid68v <sidharthv.cusat@gmail.com>
2020-09-03 21:56:00 +0530

    level 1 circuit created.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameController : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadNextLevel", 4f);
+
+    }
+    void LoadNextLevel()
+    {
+        Debug.Log("Next Level Loaded.");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
