0eed44a7da7437fb4943c32f4ed610c63f6061a1
sid68v <sidharthv.cusat@gmail.com>
2020-09-04 23:28:18 +0530

    player movements tweaked.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,12 +1,26 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public Vector2 offsets = new Vector2(2, 2);
-    public Vector2 limits = new Vector2(5, 5);
+    [SerializeField] Vector2 offsets = new Vector2(2, 2);
+    [SerializeField] Vector2 limits = new Vector2(5, 5);
+    [SerializeField] float pitchPositionFactor = -4f;
+    [SerializeField] float pitchThrowFactor = 15f;
+    [SerializeField] float yawPositionFactor = 4f;
+    //[SerializeField] float yawThrowFactor = -15f;  
+    //[SerializeField] float rollPositionFactor = 4f;
+    [SerializeField] float rollThrowFactor = -50f;
+
+
+    float horizontalThrow;
+    float verticalThrow;
+
+
     private void Start()
     {
 
@@ -14,14 +28,38 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        horizontalThrow = Input.GetAxis("Horizontal");
+        verticalThrow = Input.GetAxis("Vertical");
 
+        HandleMovement();
+        HandleRotation();
+
+    }
+
+    private void HandleRotation()
+    {
+        float pitchDueToPosition = transform.localPosition.y * pitchPositionFactor;
+        float pitchDueToThrow = verticalThrow * pitchThrowFactor;
+
+        float yawDueToPosition = transform.localPosition.x * yawPositionFactor;
+        //float yawDueToThrow = horizontalThrow * yawThrowFactor;
+
+        //float rollDueToPosition =  transform.localPosition.x* rollPositionFactor;
+        float rollDueToThrow = horizontalThrow * rollThrowFactor;
+
+        float pitch = pitchDueToPosition+pitchDueToThrow;
+        float yaw = yawDueToPosition;//+yawDueToThrow;
+        float roll = rollDueToThrow;//+rollDueToPosition;
+
+        transform.localRotation = Quaternion.Euler(pitch, yaw, roll);
+    }
+
+    private void HandleMovement()
+    {
         transform.localPosition = new Vector3(
-            Mathf.Clamp(transform.localPosition.x + horizontal*offsets.x, -limits.x, limits.x),
-            Mathf.Clamp(transform.localPosition.y + vertical*offsets.y, -limits.y, limits.y),
+            Mathf.Clamp(transform.localPosition.x + horizontalThrow * offsets.x, -limits.x, limits.x),
+            Mathf.Clamp(transform.localPosition.y + verticalThrow * offsets.y, -limits.y, limits.y),
             transform.localPosition.z
             );
-
     }
 }
