0951f23401855db6f9507c6945c5e62d5a8a9880
sid68v <sidharthv.cusat@gmail.com>
2020-09-04 16:56:42 +0530

    ship controls calibrated.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Vector2 offsets = new Vector2(2, 2);
+    public Vector2 limits = new Vector2(5, 5);
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.localPosition = new Vector3(
+            Mathf.Clamp(transform.localPosition.x + horizontal*offsets.x, -limits.x, limits.x),
+            Mathf.Clamp(transform.localPosition.y + vertical*offsets.y, -limits.y, limits.y),
+            transform.localPosition.z
+            );
+
+    }
+}
