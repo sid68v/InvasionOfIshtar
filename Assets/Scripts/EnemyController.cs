5ea0cd1e3b41abc4406e42c2fc76bcb687639ade
sid68v <sidharthv.cusat@gmail.com>
2020-09-10 23:48:33 +0530

    Added UI for score display.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,22 +7,49 @@ public class EnemyController : MonoBehaviour
 {
     [SerializeField] GameObject explosionPrefab;
     [SerializeField] float deathDelay = 1.5f;
+    [SerializeField] bool isRigidBodyAdded = true;
+    [SerializeField] int enemyDeathScore = 10;
+
+    MeshCollider meshCollider;
+    Rigidbody rigidbody;
     // Start is called before the first frame update
     void Start()
     {
-        
+        AddNonTriggerMeshCollider();
+        if (isRigidBodyAdded)
+        {
+            AddNonKinematicRigidbodyWithoutGravity();
+        }
     }
+
+
+
+    private void AddNonTriggerMeshCollider()
+    {
+        meshCollider = gameObject.AddComponent<MeshCollider>();
+        meshCollider.sharedMesh = gameObject.GetComponent<MeshFilter>().sharedMesh;
+        meshCollider.convex = true;
+        meshCollider.isTrigger = false;
+    }
+    private void AddNonKinematicRigidbodyWithoutGravity()
+    {
+        rigidbody = gameObject.AddComponent<Rigidbody>();
+        rigidbody.useGravity = false;
+    }
+
     private void OnParticleCollision(GameObject other)
     {
-        print($"{other.name} {gameObject.name}");
         GameObject explosion = Instantiate(explosionPrefab, transform.position, Quaternion.identity, transform);
         explosion.transform.localScale = Vector3.one * 10;
+
+        ScoreHandler.Instance.UpdateScore(enemyDeathScore);
+        GetComponent<MeshCollider>().enabled = false;
         Destroy(gameObject, deathDelay);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
---
+++ Assets/Scripts/ScoreHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreHandler : MonoBehaviour
+{
+    public static ScoreHandler Instance;
+
+    [SerializeField] Text scoreText;
+
+    int score = 0;
+
+
+    private void Awake()
+    {
+        if (!Instance)
+        {
+            Instance = this;
+        }
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetScoreText();
+    }
+
+    void SetScoreText()
+    {
+        scoreText.text = "score : " + score.ToString();
+    }
+    public void UpdateScore(int scorePerEnemy)
+    {
+        score += scorePerEnemy;
+        SetScoreText();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
