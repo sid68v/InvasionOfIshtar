f28bc1d7d4dcb5e8aaf9cc27fe553c6900a7d756
sid68v <sidharthv.cusat@gmail.com>
2020-09-14 16:10:42 +0530

    saving before autorotate script.

---
+++ Assets/Scripts/LookAtPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAtPlayer : MonoBehaviour
+{
+
+    GameObject playerShip;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerShip = GameObject.FindGameObjectWithTag("Player");
+        print(playerShip.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(playerShip.transform);
+    }
+}
